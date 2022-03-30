44cc7063c346ace1482fab383ce8063541dafcaa
bienzaaron <bienzaaronj@gmail.com>
2022-03-29 22:34:30 -0400

    some improvements for small screens

--- src/lib/components/family.svelte
+++ src/lib/components/family.svelte
@@ -1,5 +1,7 @@
-<img
-	src="family.png"
-	class="rounded-full ml-24 w-56 ring-2	ring-black"
-	alt="me, my wife, dog, and two cats"
-/>
+<div class="flex justify-center sm:justify-start">
+	<img
+		src="family.png"
+		class="rounded-full sm:ml-24 w-56 ring-2	ring-black"
+		alt="me, my wife, dog, and two cats"
+	/>
+</div>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,14 +1,53 @@
 <script>
 	import '../app.css';
 	import 'remixicon/fonts/remixicon.css';
+	import ClickOutside from 'svelte-click-outside';
+
+	let showMenu = false;
 </script>
 
-<div class="page px-16 lg:px-32 py-2 flex flex-col font-mono">
-	<div class="header pt-10 pb-16 flex flex-row space-x-20">
-		<a class="no-underline" href="/">Hi <span class="font-serif">👋</span></a>
-		<a class="no-underline" href="/about">About <span class="font-serif">📕</span></a>
-		<a class="no-underline" href="/blog">Blog <span class="font-serif">✍️</span></a>
-		<a class="no-underline" href="/resume.txt">Hire Me <span class="font-serif">💲</span></a>
+<div class="page px-4 lg:px-32 py-2 flex flex-col font-mono">
+	<div class="hidden sm:flex header pt-10 pb-16 flex flex-row">
+		<div class="space-x-16">
+			<a class="no-underline" href="/">Hi <span class="font-serif">👋</span></a>
+			<a class="no-underline" href="/about">About <span class="font-serif">📕</span></a>
+			<a class="no-underline" href="/blog">Blog <span class="font-serif">✍️</span></a>
+			<a class="no-underline" href="/resume.txt">Hire Me <span class="font-serif">💲</span></a>
+		</div>
+	</div>
+	<div class="sm:hidden py-4 flex flex-row justify-end">
+		<div class="flex flex-col items-end">
+			<ClickOutside
+				on:clickoutside={() => {
+					showMenu = false;
+				}}
+			>
+				<button
+					on:click={() => {
+						showMenu = !showMenu;
+					}}
+					class="rounded-sm shadow-sm py-2 px-2 h-min focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-slate-500"
+				>
+					<i class="ri-menu-fill" />
+				</button>
+			</ClickOutside>
+			<div>
+				{#if showMenu}
+					<div
+						class="absolute mt-1 right-4 px-2 w-max z-10 menu rounded-sm ring-1 ring-black ring-opacity-5 divide-y divide-slate-500 flex flex-col"
+						role="menu"
+						aria-orientation="vertical"
+					>
+						<a class="no-underline py-2" href="/">Hi <span class="font-serif">👋</span></a>
+						<a class="no-underline py-2" href="/about">About <span class="font-serif">📕</span></a>
+						<a class="no-underline py-2" href="/blog">Blog <span class="font-serif">✍️</span></a>
+						<a class="no-underline py-2" href="/resume.txt"
+							>Hire Me <span class="font-serif">💲</span></a
+						>
+					</div>
+				{/if}
+			</div>
+		</div>
 	</div>
 	<div class="page-content max-w-4xl mb-16">
 		<slot />
@@ -20,7 +59,9 @@
 		<div>
 			<a class="no-underline" href="https://github.com/bienzaaron"><i class="ri-github-line" /></a>
 			<a class="no-underline" href="https://www.npmjs.com/~ajbienz"><i class="ri-npmjs-line" /></a>
-			<a class="no-underline" href="https://www.linkedin.com/in/abienz"><i class="ri-linkedin-line" /></a>
+			<a class="no-underline" href="https://www.linkedin.com/in/abienz"
+				><i class="ri-linkedin-line" /></a
+			>
 		</div>
 	</div>
 </div>
@@ -30,6 +71,10 @@
 	.page {
 		min-height: 100vh;
 		background-color: #c0cfb4;
+	}
+
+	.menu {
+		background-color: #bcc7b4;
 	}
 
 	.page-content {
