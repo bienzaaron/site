78337daf424ae34955dc6642274be8c4f9f3bff5
bienzaaron <bienzaaronj@gmail.com>
2022-11-25 23:50:40 -0500

    lazy load image

--- src/lib/components/family.svelte
+++ src/lib/components/family.svelte
@@ -1,6 +1,7 @@
 <div class="flex justify-center sm:justify-start">
   <img
     src="family.webp"
+    loading="lazy"
     class="rounded-full sm:ml-24 w-56 ring-2	ring-black"
     alt="me, my wife, dog, and two cats"
   />
