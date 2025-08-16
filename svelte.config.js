ba966945267c88bcd78ec1bb72747895333e3b1f
bienzaaron <bienzaaronj@gmail.com>
2025-08-16 00:31:38 -0400

    don't upload test artifacts for now; some more upgrades

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,12 @@
 import adapter from "@sveltejs/adapter-static";
-import preprocess from "svelte-preprocess";
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 import { mdsvex } from "mdsvex";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   extensions: [".svelte", ".md"],
   preprocess: [
-    preprocess(),
+    vitePreprocess(),
     mdsvex({
       extensions: [".md"],
       highlight: {
