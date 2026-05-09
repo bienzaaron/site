100ce6423cfc39383ac1d234a8f49673fb1a3f8f
bienzaaron <bienzaaronj@gmail.com>
2026-05-08 21:32:03 -0400

    render blog content from markdown component

--- tests/blog-entry.spec.ts
+++ tests/blog-entry.spec.ts
@@ -1,14 +1,30 @@
 import { test, expect } from "@playwright/test";
 
-test.beforeEach(async ({ page }) => {
-  await page.goto("/blog/2-another-post/");
-});
-
 test("blog entries render correctly", async ({ page }) => {
+  await page.goto("/blog/2-another-post/");
+
   await expect(
     page.getByRole("heading", { name: "Just Messin' Around" }),
   ).toBeVisible();
   await expect(
     page.getByText("This is really just here for testing purposes"),
   ).toBeVisible();
+});
+
+test("blog entries include prerendered content without JavaScript", async ({
+  browser,
+  baseURL,
+}) => {
+  const context = await browser.newContext({ javaScriptEnabled: false });
+  const page = await context.newPage();
+
+  await page.goto(`${baseURL}/blog/2-another-post/`);
+  await expect(
+    page.getByRole("heading", { name: "Just Messin' Around" }),
+  ).toBeVisible();
+  await expect(
+    page.getByText("This is really just here for testing purposes"),
+  ).toBeVisible();
+
+  await context.close();
 });
